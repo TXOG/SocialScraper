a6e552ea3f80b77d2fe33c9ddeaee710e4664c77
Thomas Kerby <63968823+TXOG@users.noreply.github.com>
2023-02-12 15:27:54 +0000

    v1.0.0-beta

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Kivy imports
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
@@ -5,6 +6,14 @@ from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen, ScreenManager
+
+# DCF + imports
+import requests
+import webbrowser
+from selenium import webdriver
+import time
+import json
+from selenium.webdriver.chrome.options import Options
 
 kivy.require("1.11.1")
 
@@ -36,9 +45,31 @@ class Page2(Screen):
         self.add_widget(layout)
 
     def lookup(self, instance):
-        user_id = self.input.text
-        print("The user entered:", user_id)
-
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        begining_of_url = "https://lookup.guru/"
+        target_id = self.input.text
+        whole_url = begining_of_url + str(target_id)
+        driver.get(whole_url)
+        time.sleep(5)
+        images = driver.find_elements_by_tag_name('img')
+        for image in images:
+            global pfp
+            pfp = (image.get_attribute('src'))
+            break
+        img_data = requests.get(pfp).content
+        with open('pfpimage.png', 'wb') as handler:
+            handler.write(img_data)
+        filePath = "pfpimage.png"
+        searchUrl = 'https://yandex.com/images/search'
+        files = {'upfile': ('blob', open(filePath, 'rb'), 'image/jpeg')}
+        params = {'rpt': 'imageview', 'format': 'json',
+                  'request': '{"blocks":[{"block":"b-page_type_search-by-image__link"}]}'}
+        response = requests.post(searchUrl, params=params, files=files)
+        query_string = json.loads(response.content)['blocks'][0]['params']['url']
+        img_search_url = searchUrl + '?' + query_string
+        webbrowser.open(img_search_url)
 
 class SocialScraperApp(App):
     def build(self):
